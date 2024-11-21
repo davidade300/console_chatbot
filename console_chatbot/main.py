07ed409742d29020e999b813bd7a335832676afb
davidade300 <iaderinho@icloud.com>
2024-11-20 22:39:51 -0300

    pequenas modificacoes feitas, app shrek criado

--- console_chatbot/main.py
+++ console_chatbot/main.py
@@ -2,7 +2,7 @@ from openai import OpenAI  # noqa f:401
 import os
 from rich.console import Console
 from rich.markdown import Markdown
-from prompts.py_instructor import py_instructor_prompt
+from .prompts.py_instructor import py_instructor_prompt
 
 
 openai = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
@@ -11,21 +11,27 @@ chat_log = [{"role": "system", "content": py_instructor_prompt}]
 
 console = Console()
 
-print("digite 'sair' para  sair")
-while True:
-    user_input = console.input(Markdown("## usuario: "))
 
-    if user_input.lower() == "sair":
-        break
+def main():
+    print("digite 'sair' para  sair")
+    while True:
+        user_input = console.input(Markdown("## usuario: "))
 
-    chat_log.append({"role": "user", "content": user_input})
+        if user_input.lower() == "sair":
+            break
 
-    response = openai.chat.completions.create(
-        model="gpt-3.5-turbo",
-        temperature=0.7,
-        messages=chat_log,
-    )
+        chat_log.append({"role": "user", "content": user_input})
 
-    bot_response = response.choices[0].message.content
-    chat_log.append({"role": "assistant", "content": bot_response})
-    console.print(Markdown(f"## assistente: \n  {bot_response}"))
+        response = openai.chat.completions.create(
+            model="gpt-3.5-turbo",
+            temperature=0.7,
+            messages=chat_log,
+        )
+
+        bot_response = response.choices[0].message.content
+        chat_log.append({"role": "assistant", "content": bot_response})
+        console.print(Markdown(f"## assistente: \n  {bot_response}"))
+
+
+if __name__ == "__main__":
+    main()
